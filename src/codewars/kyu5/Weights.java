9904459e5b5a0ff160432e1b6fed9be26ce10cb0
petr <petr@algorim.com>
2021-03-10 08:20:01 +0100

    codewars kyu5 weights done

--- src/codewars/kyu5/Weights.java
+++ src/codewars/kyu5/Weights.java
@@ -1,70 +1,54 @@
 package codewars.kyu5;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Weights {
   public static void main(String[] args) {
-    System.out.println(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"));
+    System.out.println(orderWeight("59544965313"));
   }
 
   public static String orderWeight(String strng) {
-    int[] arr = Arrays.stream(strng.split(" ")).map(x -> x.replaceAll(" ", "")).mapToInt(Integer::valueOf).toArray();
-    List<Weight> list = Arrays.stream(arr).mapToObj(Weight::new).collect(Collectors.toList());
-    System.out.println(list);
 
-    list.sort(Comparator.comparingInt(Weight::getRecalculated).thenComparing(Weight::getRecalStr));
+    if (strng.isBlank()) return "";
+    List<Weight> list = Arrays
+            .stream(strng.split(" "))
+            .map(x -> x.replaceAll(" ", ""))
+            .mapToLong(Long::valueOf)
+            .mapToObj(Weight::new)
+            .collect(Collectors.toList());
 
+    compareAndOrder(list);
     return list.stream().map(Weight::getOriginalWeight).map(String::valueOf).collect(Collectors.joining(" "));
   }
 
+  private static void compareAndOrder(List<Weight> list) {
+    list.sort((w1, w2) -> {
+      if (w1.recalculated != w2.recalculated) {
+        return (int) (w1.recalculated - w2.recalculated);
+      } else {
+        return String.valueOf(w1.originalWeight).compareTo(String.valueOf(w2.originalWeight));
+      }
+    });
+  }
+
   static class Weight {
-    public int originalWeight;
-    public int recalculated;
-    public String recalStr;
+    public long originalWeight;
+    public long recalculated;
 
-    public Weight(int nr) {
+    public Weight(long nr) {
       this.originalWeight = nr;
-      this.recalculated = Arrays.stream(String.valueOf(nr).split("")).mapToInt(Integer::valueOf).sum();
-      this.recalStr = String.valueOf(recalculated);
+      this.recalculated = Arrays.stream(String.valueOf(nr).split("")).mapToLong(Long::valueOf).sum();
     }
 
-    public int getOriginalWeight() {
+    public long getOriginalWeight() {
       return originalWeight;
-    }
-
-    public String getRecalStr() {
-      return recalStr;
-    }
-
-    public int getRecalculated() {
-      return recalculated;
     }
 
     @Override
     public String toString() {
-      return "originalWeight=" + originalWeight +
-              ", recalculated=" + recalculated;
+      return String.valueOf(recalculated);
     }
   }
-
-  /*statsList.sort(Comparator.comparingInt(TeamStats::getPoints).reversed()
-            .thenComparingInt(TeamStats::getAgainstScore).reversed()
-            .thenComparingInt(TeamStats::getBalance).reversed());*/
-
-  /*private static void compareResultsAndOrderTeams(List<Team> teamList) {
-    teamList.sort((o1, o2) -> {
-      if (o1.getPoints() != o2.getPoints()) {
-        return o2.getPoints() - o1.getPoints();
-      } else if (o1.plusMinus() != o2.plusMinus()) {
-        return o2.plusMinus() - o1.plusMinus();
-      } else if (o1.getScored() != o2.getScored()) {
-        return o2.getScored() - o1.getScored();
-      } else {
-        return o1.getName().compareToIgnoreCase(o2.getName());
-      }
-    });
-  }*/
 }
