da672c7ae2b43790d060c71806944615f74bb6e9
petr <petr@algorim.com>
2021-03-12 18:48:28 +0100

    kyu5 valid parentheses

--- src/hackerRank/SherlockAnagram.java
+++ src/hackerRank/SherlockAnagram.java
@@ -4,17 +4,18 @@ import java.util.HashMap;
 
 public class SherlockAnagram {
   public static void main(String[] args) {
-    System.out.println(sherlockAndAnagrams("abba"));
+    System.out.println(sherlockAndAnagrams("abbarotor"));
   }
 
   private static int sherlockAndAnagrams(String str) {
-    int result = str.length();
-    HashMap<String, Integer> map = new HashMap<>();
-    for (int i = 0; i < str.length()-1; i++) {
-      for (int j = i; j < str.length(); j++) {
-        String sub = str.substring(i,j);
-        if (map.containsKey(sub)) map.put(sub, map.get(sub)+1);
-        else map.put(sub,1);
+    int result = 0;
+
+    // if I need pairs, then I can have longest pair as long as half size of the word
+    // since each lenght between 2-half is needed I have to iterate this
+    //I start at length 1, and check if current letter has a duplicate
+    for (int i = 1; i < str.length()/2; i++) {
+      for (int j = 1; j < 8; j++) {
+
       }
     }
     return 0;
