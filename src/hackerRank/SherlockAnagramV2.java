60ff8641c3bbbf3897450286084eda24638aa7cc
petr <petr@algorim.com>
2021-03-20 17:57:59 +0100

    hackerrank sherlock anagram works but timeout

--- src/hackerRank/SherlockAnagramV2.java
+++ src/hackerRank/SherlockAnagramV2.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class SherlockAnagram {
+public class SherlockAnagramV2 {
   public static void main(String[] args) {
     System.out.println(sherlockAndAnagrams("abba"));
   }
