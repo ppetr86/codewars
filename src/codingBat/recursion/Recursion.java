1327f0328e74d52056967e62b85d5a2d8559966d
petr <petr@algorim.com>
2021-04-14 23:16:33 +0200

    recursion on codingbat

--- src/codingBat/recursion/Recursion.java
+++ src/codingBat/recursion/Recursion.java
@@ -213,10 +213,10 @@ public class Recursion {
     boolean holder2 = groupSum5(start + 1, nums, target - nums[start]);
     boolean startsWith5 = nums[start] % 5 == 0;
     if (startsWith5) {
-      if(start <= nums.length - 2 && nums[start+1] == 1)
-        return groupSum5(start+2, nums, target - nums[start]);
+      if (start <= nums.length - 2 && nums[start + 1] == 1)
+        return groupSum5(start + 2, nums, target - nums[start]);
       else
-        return groupSum5(start+1,nums,target - nums[start]);
+        return groupSum5(start + 1, nums, target - nums[start]);
 
     }
 
@@ -225,6 +225,31 @@ public class Recursion {
 
     boolean holder = groupSum5(start + 1, nums, target);
     if (holder) return true;
+
+    return false;
+  }
+
+  //Given an array of ints, is it possible to choose a group of some of the ints, such that the group
+  // sums to the given target, with this additional constraint: if there are numbers in the array that
+  // are adjacent and the identical value, they must either all be chosen, or none of them chosen. For
+  // example, with the array {1, 2, 2, 2, 5, 2}, either all three 2's in the middle must be chosen or
+  // not, all as a group. (one loop can be used to find the extent of the identical values).
+  public boolean groupSumClump(int start, int[] nums, int target) {
+    if (start >= nums.length) return target == 0;
+
+    int i = start;
+    int sum = 0;
+
+    while(i < nums.length && nums[start] == nums[i]) {
+      sum += nums[i];
+      i++;
+    }
+
+    if(groupSumClump(i, nums, target - sum))
+      return true;
+
+    if(groupSumClump(i, nums, target))
+      return true;
 
     return false;
   }
